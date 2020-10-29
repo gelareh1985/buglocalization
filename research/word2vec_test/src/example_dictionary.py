74d65de2f16fd5dfc77b4453cff479448262f261
gelareh1985 <meydanipour.gelareh@gmail.com>
2020-10-29 22:24:09 +0100

    read all files from directory and create a single set of unique lines

--- research/word2vec_test/src/example_dictionary.py
+++ research/word2vec_test/src/example_dictionary.py
@@ -1,45 +1,67 @@
 import os
+import numpy as np
 from word_to_vec import generate_dictinoary_data
-
+from docutils.utils.math.math2html import file
 
 
 print("Hello World!")
 
-
+global p
 p='D:\\files_MDEAI_original\\Datasets\\DataSet_20200925144220_with_tabs\\DataSet_20200925144220\\evaluation\\'
-#path=r'D:\files_MDEAI_original\Datasets\DataSet_20200925144220_with_tabs\DataSet_20200925144220\training\00002_bug_12000_version_2c9cb94dc84956e5c7f0db27e02f01d02c3e4f02.nodelist'
+#path=r'D:\files_MDEAI_original\Datasets\DataSet_20200925144220_with_tabs\DataSet_20200925144220\evaluation\all_table_data.txt'
+global temp_file
+global table_data
+global strline
+global text
+#global filename
+#global filepath
 
+file_with_unique_lines="uniqueset.txt"
 
 def load_table(filepath):
     table_data=[]
     with open(filepath) as f:
-         #content=f.read()
          #print(filename)
          #table_data.append(content)
          #print(content)
         for i, line in enumerate(f):
-             info = line.strip().split('\t')
-             table_data.append(info)
+            info = line.strip().split('\t')
+            table_data.append(info)
+            #print(line)
+            
     return table_data
-                          
-   
-def save_table(out_file):
-    with open('tbl.txt','w') as fp:
-        for row in out_file:
-            fp.write("%s\n" % row)
-            print("finished!")
-            
-            
-tbl_list=[]
-for filename in os.listdir(p):
-        filepath=os.path.join(p,filename)
-        if filename.endswith(".nodelist"):
-           table=load_table(filepath)
-           tbl_list.append(table)    
-   
-       
-save_table(tbl_list)           
-    
+                                  
 
-            
-          
+temp_file="table_of_string.txt"
+text=[]
+for filename in os.listdir(p):
+    filepath=os.path.join(p,filename)
+    if filename.endswith(".nodelist"):
+       
+       with open(temp_file,"a")as f: 
+           table=load_table(filepath)
+           for line in table:
+               #print('table lines: \t',line[0],'\t', line[1])
+               strline='{}{}\n'.format(line[0],line[1])
+               #print(strline)
+               text.append(strline)
+               f.write(strline)
+            #print("Finished collecting data from all files!")        
+            #return text 
+
+print("length of all text: ",len(text))
+
+        
+iterated_items = set()
+for value in text: 
+    if value not in iterated_items: 
+        if len(value)!=0: 
+            iterated_items.add(value)
+               
+print('length of set: ',len(iterated_items))
+#   
+# with open(file_with_unique_lines,"a") as f:
+#       #print(txtset)
+#       for l in iterated_items: 
+#           f.write(l)
+#                 
