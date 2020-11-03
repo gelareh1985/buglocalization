a604c64f0e78cf7e8668fa4586af8f1d184ecf87
gelareh1985 <meydanipour.gelareh@gmail.com>
2020-11-03 19:55:59 +0100

    creating dictionary of data and binary vectors

--- research/word2vec_test/src/example_dictionary.py
+++ research/word2vec_test/src/example_dictionary.py
@@ -2,15 +2,16 @@ import os
 import numpy as np
 import re
 from nltk.tokenize import RegexpTokenizer
-import pickle
-
+from collections import OrderedDict
 from word_to_vec import generate_dictinoary_data
+from bokeh.util.terminal import dim
 
 print("Hello World!")
 
-global p
-p='D:\\files_MDEAI_original\\Datasets\\DataSet_20200925144220_with_tabs\\DataSet_20200925144220\\evaluation\\'
 global table_data
+global p1
+p1='D:\\files_MDEAI_original\\Datasets\\DataSet_20200925144220_with_tabs\\DataSet_20200925144220\\evaluation\\'
+p2='D:\\files_MDEAI_original\\Datasets\\DataSet_20200925144220_with_tabs\\DataSet_20200925144220\\training\\'
 
 #---------------------------------------------------------------
 
@@ -19,93 +20,112 @@ def load_table(filepath):
     with open(filepath) as f:
         for i, line in enumerate(f):
             info = line.strip().split('\t')
+            info=info[1].replace('"',"")
             table_data.append(info)
-            #print(info)
             
     return table_data
    
 def create_unique_set(list):   
     iterated_items = set()
+    list=filter(None, list)
     for value in list: 
-        if value not in iterated_items and len(value)!=0: 
+        
+        if value not in iterated_items and value.isspace()==False: 
             iterated_items.add(value)
                    
-    print('length of set: ',len(iterated_items))
+    #print('length of set: ',len(iterated_items))
     return iterated_items
 
 def save_file(save_path,list):
     with open(save_path,"w") as f:
          for line in list:
              f.write(line)                                  
+    f.close() 
 
 def generate_dictinoary(text):
-    row_to_index=dict()
-    count=0
-    vocab_size=0
-    for row in text:
-        row=row.lower()
-        if row_to_index.get(row)==None:
-            row_to_index.update({row:count})
-            count+=1
-    vocab_size=len(row_to_index)
-    return row_to_index, vocab_size        
+    row_to_index=OrderedDict({index:i for i, index in enumerate(text)})
+    vocab_size=len(row_to_index)  
+    return row_to_index,vocab_size       
 
 #---------------------------------------------------------------
             
-table_dt="data_table.txt"
 text1=[]
-
-for filename in os.listdir(p):
-    filepath=os.path.join(p,filename)
+for filename in os.listdir(p1):
+    filepath=os.path.join(p1,filename)
     
     if filename.endswith(".nodelist"):
        table=load_table(filepath)
-       
        for line in table:
-           tokenizer=RegexpTokenizer('[A-Za-z]+')
-           st=tokenizer.tokenize(line[1])
-
-           strline=''
-           for l in st:
-               strline=strline+' '+l
-           text1.append(strline+'\n')
-               
- 
+            tokenizer=RegexpTokenizer('[A-Za-z]+')
+            st=tokenizer.tokenize(line)
+            strln=" ".join(st)+'\n'
+            text1.append(strln)
+            
 print("length of all text: ",len(text1))
-save_file(table_dt,text1)
+save_file("data_table.txt",text1)
 
-txtfile1="list1.txt"
 list1=create_unique_set(text1)  
-save_file(txtfile1,list1)
- 
+save_file("list1.txt",list1)
 
-row_to_index,vocab_size= generate_dictinoary(list1)
-
-dict_file = open("data.pkl", "wb")
-pickle.dump(row_to_index, dict_file) #save dictionary data
-dict_file.close()
-
-# dict_file = open("data.pkl", "rb")
-# output = pickle.load(dict_file) # load dictionary data
-# print(output)
- 
-file_sample='00002_bug_12000_version_2c9cb94dc84956e5c7f0db27e02f01d02c3e4f02.nodelist'
-txtfile2="list2.txt"
-text2=[]
-
-table2=load_table(file_sample)
-for line in table2:
+dictionary_list1,vocabulary_size1= generate_dictinoary(list1)
+print("vocabulary_size1: ", vocabulary_size1)
     
-    tokenizer=RegexpTokenizer('[A-Za-z]+')
-    st=tokenizer.tokenize(line[1])
-
-    strline=''
-    for l in st:
-        strline=strline+' '+l
-    text2.append(strline+'\n')
+with open("dictionary_file_corpus.txt",'w') as f:
+    for keys,values in dictionary_list1.items():
+        f.write(str(values)+'\t'+keys)
+                    
+i=1
+listoflists=[]
+for filename in os.listdir(p2):
+    
+    filepath=os.path.join(p2,filename)
+    if filename.endswith(".nodelist"):
+        table2=load_table(filepath)
+        text2=[]
+        for line in table2:
+            
+            tokenizer=RegexpTokenizer('[A-Za-z]+')
+            st=tokenizer.tokenize(line)
+            strln=" ".join(st)+'\n'
+            text2.append(strln)
    
-list2=create_unique_set(text2)
-save_file(txtfile2,list2)
+        list2=create_unique_set(text2)
+        print('length of file '+str(i)+': '+str(len(list2)))
+        
+        train_file_name="train_file_"+str(i)+".txt"
+        save_file(train_file_name,list2)
+        
+        dictionary_list2,vocabulary_size2= generate_dictinoary(list2)
+        print("vocabulary_size2: ", vocabulary_size2)
+        
+        encoded_l=np.zeros(vocabulary_size1,dtype=np.uint8)
 
+        j=0
+        sublist=[]
+        for line in dictionary_list1.keys():
+            if line in dictionary_list2:
+                encoded_l[j]=1
+                sublist.append(str(encoded_l[j]))
+            else:
+                encoded_l[j]=0
+                sublist.append(str(encoded_l[j]))
+            j+=1
+        listoflists.append(sublist)    
 
-              
+        encoded_file_name="encoded_file_"+str(i)+".txt"  
+        np.savetxt(encoded_file_name,encoded_l, fmt="%d")
+                
+        dictionary_file_name="dictionary_file_"+str(i)+".txt"        
+        with open(dictionary_file_name,'w') as f:
+            for keys,values in dictionary_list2.items():
+                f.write(str(values)+'\t'+keys) 
+                
+    i=i+1
+
+print('length of listoflists: ', len(listoflists), '  length of sublist: ', len(sublist))
+
+with open('list_of_lists.txt','w') as f:
+    for item1 in  listoflists:
+        for item2 in sublist:
+         f.write(item2+' ')
+
