b8033a0a3c55b507fef4a0d733a79737fb654267
Manuel Ohrndorf <manuel.ohrndorf@gmx.net>
2021-01-18 18:56:46 +0100

    fixed reading of feature size

--- plugins/org.sidiff.bug.localization.prediction/src/models_bug_localization_model.py
+++ plugins/org.sidiff.bug.localization.prediction/src/models_bug_localization_model.py
@@ -1,36 +1,58 @@
 import ntpath
 import os
+from typing import Union, List, Tuple
 
-from stellargraph.layer import GraphSAGE, link_classification
-from stellargraph.mapper import GraphSAGELinkGenerator
-from tensorflow import keras
+from pandas.core.frame import DataFrame  # type: ignore
+from stellargraph.layer import GraphSAGE, link_classification  # type: ignore
+from stellargraph.mapper import GraphSAGELinkGenerator  # type: ignore
+from tensorflow import keras  # type: ignore
 
-import numpy as np
-import pandas as pd
-import stellargraph as sg
+import numpy as np  # type: ignore
+import pandas as pd  # type: ignore
+import stellargraph as sg  # type: ignore
 
-positve_samples_path = r"C:\Users\manue\git\buglocalization\plugins\org.sidiff.bug.localization.embedding.properties\testdata\positivesamples_5000/"
-negative_samples_path = r"C:\Users\manue\git\buglocalization\plugins\org.sidiff.bug.localization.embedding.properties\testdata\negativesamples_5000/"
+###################################################################################################
+# Environmental Information
+###################################################################################################
+
+positve_samples_path:str = r"C:\Users\manue\git\buglocalization\plugins\org.sidiff.bug.localization.embedding.properties\testdata\positivesamples_5000/"
+negative_samples_path:str = r"C:\Users\manue\git\buglocalization\plugins\org.sidiff.bug.localization.embedding.properties\testdata\negativesamples_5000/"
+
+binary_featurenodelist:bool = True
+
+###################################################################################################
+# Data Processing
+###################################################################################################
 
 class DataSetSample:
     
     def __init__(self):
-        self.sample_nodes = None
-        self.sample_edges = None
-        self.sample_bug_location_pairs = None
-        self.sample_testcase_labels = None
+        
+        # Nodes of the bug localization graph (excerpt).
+        self.sample_nodes:List[DataFrame] = None
+        
+        # Edges of the bug localization graph (excerpt).
+        self.sample_edges:List[DataFrame] = None
+        
+        # Pairs of node IDs: (Bug Report, Location)
+        self.sample_bug_location_pairs:List[Tuple[str, str]] = None
+        
+        # 0 or 1 for each bug location pair: 0 -> positive sample, 1 -> negative sample
+        self.sample_testcase_labels:np.ndarray = None
         
         
 class DataSetLoader:
     
-    def __init__(self, positve_samples_path, negative_samples_path):
-        self.positve_samples_path = positve_samples_path
-        self.negative_samples_path = negative_samples_path
-        self.feature_size = self.load_feature_size()
-        self.positive_sample_size = len(self.get_sample_paths(self.positve_samples_path))
-        self.negative_sample_size = len(self.get_sample_paths(self.negative_samples_path))
+    def __init__(self, positve_samples_path:str, negative_samples_path:str, binary_featurenodelist:bool):
+        self.positve_samples_path:str = positve_samples_path
+        self.negative_samples_path:str = negative_samples_path
+        self.binary_featurenodelist:bool = binary_featurenodelist
+        
+        self.feature_size:int = self.load_feature_size()
+        self.positive_sample_size:int = len(self.get_sample_paths(self.positve_samples_path))
+        self.negative_sample_size:int = len(self.get_sample_paths(self.negative_samples_path))
     
-    def load_dataset(self, start_with_sample=0, number_of_sample=None, log=False):
+    def load_dataset(self, start_with_sample:int=0, number_of_sample:int=-1, log:bool=False) -> List[DataSetSample]:
     
         # Collect all graphs from the given folder:
         
@@ -52,7 +74,7 @@ class DataSetLoader:
                     
             index += 1
             
-            if number_of_sample is not None and len(all_samples) == number_of_sample:
+            if number_of_sample != -1 and len(all_samples) == number_of_sample:
                 break
         
         # Read all samples and create unique node IDs:
@@ -102,25 +124,25 @@ class DataSetLoader:
         
         return dataset_samples
     
-    def get_sample_paths(self, folder, count=None):
+    def get_sample_paths(self, folder:str, count:int=-1) -> List[str]:
         files_samples = []
         
         for filename in os.listdir(folder):
             if filename.endswith(".edgelist"):
                 edge_list_path = folder + filename
                 files_samples.append(edge_list_path)
-            if count is not None and len(files_samples) == count:
+            if count != -1 and len(files_samples) == count:
                 break
         
         return files_samples
     
-    def get_edge_list_path(self, graph_path, graph_filename):
+    def get_edge_list_path(self, graph_path:str, graph_filename:str) -> str:
         return graph_path + "/" + graph_filename + ".edgelist"
     
-    def get_featurenodelist_path(self, graph_path, graph_filename):
+    def get_featurenodelist_path(self, graph_path:str, graph_filename:str) -> str:
         return graph_path + "/features/" + graph_filename + ".featurenodelist"
     
-    def load_feature_size(self):
+    def load_feature_size(self) -> int:
         
         # read from first positive sample...
         file_positive_sample = self.get_sample_paths(self.positve_samples_path, 1)
@@ -133,9 +155,14 @@ class DataSetLoader:
         featurenodelist_path = self.get_featurenodelist_path(graph_path, graph_filename)
         
         # Assumes Tables: index, n-feature, tag
-        return len(self.load_featurenodelist(featurenodelist_path).columns) - 2
+        if self.binary_featurenodelist:
+            # subtract: tag column
+            return len(self.load_featurenodelist(featurenodelist_path).columns) - 1
+        else:
+            # subtract: index and tag column
+            return len(self.load_featurenodelist(featurenodelist_path, with_columns=False).columns) - 2
     
-    def load_nodes(self, node_list_path, graph_number):
+    def load_nodes(self, node_list_path:str, graph_number:str) -> DataFrame:
         
         # Load data:
         node_data = self.load_featurenodelist(node_list_path)
@@ -146,26 +173,30 @@ class DataSetLoader:
         
         return node_data
     
-    def load_featurenodelist(self, node_list_path, binary=True):
-        if binary:
+    def load_featurenodelist(self, node_list_path:str, with_columns:bool=True) -> DataFrame:
+        if self.binary_featurenodelist:
             return pd.read_pickle(node_list_path, compression="zip")
         else:
             # Column names:
             # TODO: Just create the header once for all files...
-            node_data_columns = []
-            node_data_columns.append("index")
-            
-            for feature_num in range(0, self.feature_size):
-                node_data_columns.append("feature" + str(feature_num))
-            
-            node_data_columns.append("tag")
+            if with_columns:
+                node_data_columns = []
+                node_data_columns.append("index")
                 
-            node_data = pd.read_table(node_list_path, names=node_data_columns)
-            node_data.set_index("index", inplace=True)
+                for feature_num in range(0, self.feature_size):
+                    node_data_columns.append("feature" + str(feature_num))
+                
+                node_data_columns.append("tag")
+                    
+                node_data = pd.read_table(node_list_path, names=node_data_columns)
+                node_data.set_index("index", inplace=True)
+                return node_data
+            else:
+                node_data = pd.read_table(node_list_path)
+                return node_data
             
-            return node_data
     
-    def load_edges(self, edge_list_path, graph_number):
+    def load_edges(self, edge_list_path:str, graph_number:str) -> DataFrame:
         edge_list_col_names = ["source", "target"]
         edge_data = pd.read_table(edge_list_path, names=edge_list_col_names)
         
@@ -175,12 +206,12 @@ class DataSetLoader:
         
         return edge_data
     
-    def add_prefix(self, prefix, index):
+    def add_prefix(self, prefix:str, index:int) -> str:
         return str(prefix) + "_" + str(index)
     
-    def extract_bug_locations(self, node_data):
+    def extract_bug_locations(self, node_data:DataFrame) -> List[Tuple[str, str]]:
         bug_locations = []
-        bug_report_node = None
+        bug_report_node:str = ""
         
         for node_index, node_row in node_data.iterrows():
             tag = node_row["tag"]
@@ -188,17 +219,18 @@ class DataSetLoader:
             if (tag == "# REPORT"):
                 bug_report_node = node_index
             elif (tag == "# LOCATION"):
-                bug_location_node = node_index
-                bug_locations.append([bug_report_node, bug_location_node])
+                bug_location_node:str = node_index
+                bug_location_pair:Tuple[str, str] = (bug_report_node, bug_location_node)
+                bug_locations.append(bug_location_pair)
         
-        if (bug_report_node is None):
+        if not bug_report_node:
             raise Exception("Error: Bug report node not found!")
         
         # Remove the column 'tag' - it is not a numerical column:
         node_data.drop("tag", inplace=True, axis=1)
         return bug_locations
     
-    def remove_bug_location_edges(self, edge_data, bug_locations):
+    def remove_bug_location_edges(self, edge_data:DataFrame, bug_locations:List[Tuple[str, str]]):
         bug_location_edges = []
         
         for bug_location in bug_locations:
@@ -212,10 +244,10 @@ class DataSetLoader:
     
 class DataSetSplitter:
     
-    def __init__(self, dataset_loader):
-        self.dataset_loader = dataset_loader
+    def __init__(self, dataset_loader:DataSetLoader):
+        self.dataset_loader:DataSetLoader = dataset_loader
         
-    def load_samples(self, start_with_sample=0, number_of_sample=None, log=False):
+    def load_samples(self, start_with_sample:int=0, number_of_sample:int=-1, log:bool=False) -> Union[sg.StellarGraph, List[Tuple[str, str]], np.ndarray, sg.StellarGraph, List[Tuple[str, str]], np.ndarray]:
         dataset_samples = dataset_loader.load_dataset(start_with_sample, number_of_sample, log)
         
         # Split training and test data:
@@ -238,8 +270,8 @@ class DataSetSplitter:
         # Train/Test -> Graph, Node-Pairs, Edge-Exists-Label (positive=1/negative=0 sample)
         return G_train, bug_location_pairs_train, testcase_labels_train, G_test, bug_location_pairs_test, testcase_labels_test
 
-    def concat_samples(self, dataset_samples):
-        nodes= []
+    def concat_samples(self, dataset_samples:List[DataSetSample]) -> Union[List[DataFrame], List[DataFrame], List[Tuple[str, str]], np.ndarray]:
+        nodes = []
         edges = []
         bug_location_pairs = []
         testcase_labels = np.zeros(0)
@@ -258,10 +290,13 @@ class DataSetSplitter:
         
         return pd_nodes, pd_edges, bug_location_pairs, testcase_labels
 
+###################################################################################################
+# AI Model
+###################################################################################################
     
 class BugLocalizationAIModelBuilder:
     
-    def create_model(self, num_samples, layer_sizes, feature_size):
+    def create_model(self, num_samples:List[int], layer_sizes:List[int], feature_size:int) -> keras.Model:
         graphsage = GraphSAGE(
             n_samples=num_samples,
             layer_sizes=layer_sizes,
@@ -291,14 +326,15 @@ class BugLocalizationAIModelBuilder:
     
 class BugLocalizationAIModelTrainer:
     
-    def __init__(self, model, num_samples, batch_size, epochs):
+    def __init__(self, dataset_splitter:DataSetSplitter, model:keras.Model, num_samples:List[int], batch_size:int, epochs:int):
+        self.dataset_splitter = dataset_splitter
         self.model = model
         self.num_samples = num_samples
         self.batch_size = batch_size
         self.epochs = epochs
     
-    def train_model(self, start_with_sample=0, number_of_sample=None, evaluate_before=True, evaluate_after=True, log=True):
-        G_train, bug_location_pairs_train, edge_labels_train, G_test, bug_location_pairs_test, edge_labels_test = dataset_splitter.load_samples(start_with_sample, number_of_sample, log)
+    def train_model(self, start_with_sample:int=0, number_of_sample:int=-1, evaluate_before:bool=True, evaluate_after:bool=True, log:bool=True):
+        G_train, bug_location_pairs_train, edge_labels_train, G_test, bug_location_pairs_test, edge_labels_test = self.dataset_splitter.load_samples(start_with_sample, number_of_sample, log)
         
         train_gen = GraphSAGELinkGenerator(G_train, self.batch_size, self.num_samples)
         train_flow = train_gen.flow(bug_location_pairs_train, edge_labels_train, shuffle=True)
@@ -334,55 +370,57 @@ class BugLocalizationAIModelTrainer:
             print("\nTest Set Metrics of the trained model:")
             for name, val in zip(model.metrics_names, test_metrics):
                 print("\t{}: {:0.4f}".format(name, val))
-        
-###################################################################################################
-# Create Training and Test Data:
-###################################################################################################
 
 
-# Collect all graphs from the given folder:
-dataset_loader = DataSetLoader(positve_samples_path, negative_samples_path)
-dataset_splitter = DataSetSplitter(dataset_loader)
-
-###################################################################################################
-# Create AI Model:
-###################################################################################################
-
-num_samples = [20, 10]
-layer_sizes = [20, 20]
-
-bug_localization_model_builder = BugLocalizationAIModelBuilder()
-model = bug_localization_model_builder.create_model(num_samples, layer_sizes, dataset_loader.feature_size)
- 
-###################################################################################################
-# Train and Evaluate AI Model:
-###################################################################################################
-
-batch_size_per_training = 20  # see bug_samples_per_training
-epochs_per_training = 1  # see global_epochs 
-
-bug_localization_model_trainer = BugLocalizationAIModelTrainer(
-    model=model,
-    num_samples=num_samples,
-    batch_size=batch_size_per_training,
-    epochs=epochs_per_training)
-
-# TODO: Shuffle samples during global epochs?
-global_epochs = 20
-
-# Note: A sample is pair of a bug report and model location, each "bug sample" contains one bug report and multiple locations.
-bug_samples_per_training = 20
-bug_sample_number = dataset_loader.negative_sample_size + dataset_loader.negative_sample_size
-
-for global_epoch in range(global_epochs):
-    current_sample = 0
+if __name__ == '__main__':
     
-    while (current_sample < bug_sample_number):
-        bug_localization_model_trainer.train_model(
-            start_with_sample=current_sample,
-            number_of_sample=bug_samples_per_training,
-            evaluate_before=(global_epoch == 0),  # first epoch?
-            evaluate_after=(global_epoch == global_epochs),  # last epoch?
-            log=True)
-        current_sample += bug_samples_per_training
-
+    ###################################################################################################
+    # Create Training and Test Data:
+    ###################################################################################################
+    
+    # Collect all graphs from the given folder:
+    dataset_loader = DataSetLoader(positve_samples_path, negative_samples_path, binary_featurenodelist)
+    dataset_splitter = DataSetSplitter(dataset_loader)
+    
+    ###################################################################################################
+    # Create AI Model:
+    ###################################################################################################
+    
+    num_samples = [20, 10]
+    layer_sizes = [20, 20]
+    
+    bug_localization_model_builder = BugLocalizationAIModelBuilder()
+    model = bug_localization_model_builder.create_model(num_samples, layer_sizes, dataset_loader.feature_size)
+     
+    ###################################################################################################
+    # Train and Evaluate AI Model:
+    ###################################################################################################
+    
+    batch_size_per_training = 20  # see bug_samples_per_training
+    epochs_per_training = 1  # see global_epochs 
+    
+    bug_localization_model_trainer = BugLocalizationAIModelTrainer(
+        dataset_splitter=dataset_splitter,
+        model=model,
+        num_samples=num_samples,
+        batch_size=batch_size_per_training,
+        epochs=epochs_per_training)
+    
+    # TODO: Shuffle samples during global epochs?
+    global_epochs = 20
+    
+    # Note: A sample is pair of a bug report and model location, each "bug sample" contains one bug report and multiple locations.
+    bug_samples_per_training = 20
+    bug_sample_number = dataset_loader.negative_sample_size + dataset_loader.negative_sample_size
+    
+    for global_epoch in range(global_epochs):
+        current_sample = 0
+        
+        while (current_sample < bug_sample_number):
+            bug_localization_model_trainer.train_model(
+                start_with_sample=current_sample,
+                number_of_sample=bug_samples_per_training,
+                evaluate_before=(global_epoch == 0),  # first epoch?
+                evaluate_after=(global_epoch == global_epochs),  # last epoch?
+                log=True)
+            current_sample += bug_samples_per_training      
