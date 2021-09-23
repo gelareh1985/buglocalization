570951bc051a03c22c358fce0ed3c45795a8f0c9
Manuel Ohrndorf <manuel.ohrndorf@gmx.net>
2021-09-23 21:06:34 +0200

    node self embedding with word ranking

--- plugins/org.sidiff.bug.localization.prediction/src/node_self_embedding.py
+++ plugins/org.sidiff.bug.localization.prediction/src/node_self_embedding.py
@@ -1,6 +1,11 @@
-#from buglocalization.selfembedding.dictionary.node_self_embedding_word_ranking import compute_node_self_embedding
+# SOWE of truncated, ranked words:
+from buglocalization.selfembedding.dictionary.node_self_embedding_word_ranking import compute_node_self_embedding
+
+# SOWE of similar words:
 # from buglocalization.selfembedding.dictionary.node_self_embedding_similar_words import compute_node_self_embedding
-from buglocalization.selfembedding.dictionary.node_self_embedding_dictionary_test import compute_node_self_embedding
+
+# Check the stored embeddings:
+# from buglocalization.selfembedding.dictionary.node_self_embedding_dictionary_test import compute_node_self_embedding
 
 if __name__ == '__main__':
     compute_node_self_embedding()
